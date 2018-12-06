1f8302b4e4974bd575354d03fc1e3ff857baf722
foolishe <sayhimage@outlook.com>
2018-12-05 21:05:57 -0800

    what

--- hello.py
+++ hello.py
@@ -1,8 +1,4 @@
 print('hello world')
 print('hello agian')
 print('hello')
-<<<<<<< HEAD
-print ('what')
-=======
-print('hello,hello')
->>>>>>> 4aaeeae8bed2548add2b7e4f9f47251a0e86607f
+print ('what222')
