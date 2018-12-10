963af54a30d76d9c91650f6ac87f5d92235aa827
foolishe <sayhimage@outlook.com>
2018-12-10 07:08:39 -0800

    "program is fun"

--- Alien_Invasion/alien_invasion.py
+++ Alien_Invasion/alien_invasion.py
@@ -2,14 +2,16 @@ import sys,pygame
 from settings import Settings
 from ship import Ship
 import game_functions as gf
+from pygame.sprite import Group
 
 def run_game():
     pygame.init()
     aliens_settings=Settings()
     screen=pygame.display.set_mode((aliens_settings.screen_width,aliens_settings.screen_height))
     pygame.display.set_caption('Alien Incasion')
-    ship=Ship(screen)
+    ship=Ship(screen,aliens_settings)
+    bullets=Group()
     while True: #每次循环都会自动重绘屏幕
-        gf.check_events() #监听鼠标键盘
-        gf.update_screen(aliens_settings,screen,ship)#更新屏幕上的图像,并切换到新屏幕
+        gf.check_events(aliens_settings,screen,ship,bullets)
+        gf.update_screen(aliens_settings,screen,ship,bullets)#更新屏幕上的图像,并切换到新屏幕
 run_game()
--- Alien_Invasion/game_functions.py
+++ Alien_Invasion/game_functions.py
@@ -1,11 +1,43 @@
 import sys,pygame
+from ship import Ship
+from bullet import Bullet
 
-def check_events():
+
+def check_events(aliens_settings,screen,ship,bullets):
     for event in pygame.event.get():
         if event.type==pygame.QUIT:
             sys.exit()
+        elif event.type==pygame.KEYDOWN:
+            check_events_keydown(event,ship,aliens_settings,screen,bullets)
+        elif event.type==pygame.KEYUP:
+            check_events_keyup(event,ship)
 
-def update_screen(aliens_settings,screen,ship):
-    screen.fill(aliens_settings.bg_color) #设置背景颜色
-    ship.blitme() #绘制飞船
+def check_events_keydown(event,ship,aliens_settings,screen,bullets):
+    if event.key==pygame.K_SPACE:
+        ship.fire=True
+    if event.key==pygame.K_RIGHT:
+        ship.moving_right=True
+    if event.key==pygame.K_LEFT:
+        ship.moving_left=True
+
+def check_events_keyup(event,ship):
+    if event.key==pygame.K_SPACE:
+        ship.fire=False
+        ship.bullet_time=0
+    if event.key==pygame.K_RIGHT:
+        ship.moving_right=False
+    if event.key==pygame.K_LEFT:
+        ship.moving_left=False
+
+def update_screen(aliens_settings,screen,ship,bullets):
+    screen.fill(aliens_settings.bg_color)
+    ship.update()
+    ship.blitme()
+    bullets.update()
+    for bullet in bullets.copy():
+        if bullet.rect.bottom<=0:
+            bullets.remove(bullet)
+    ship.fire_bullet(ship,bullets)
+    for bullet in bullets.sprites():
+        bullet.draw_bullet()
     pygame.display.flip() #让最近绘制的屏幕可见
