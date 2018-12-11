58019f884eca7e2dd08022ec1b29b2ffac487a25
foolishe <sayhimage@outlook.com>
2018-12-11 09:02:45 -0800

    'keep going'

--- Alien_Invasion/alien_invasion.py
+++ Alien_Invasion/alien_invasion.py
@@ -3,6 +3,7 @@ from settings import Settings
 from ship import Ship
 import game_functions as gf
 from pygame.sprite import Group
+from alien import Alien
 
 def run_game():
     pygame.init()
@@ -11,7 +12,9 @@ def run_game():
     pygame.display.set_caption('Alien Incasion')
     ship=Ship(screen,aliens_settings)
     bullets=Group()
+    aliens=Group()
+    gf.create_fleet(aliens_settings,screen,aliens,ship)
     while True: #每次循环都会自动重绘屏幕
         gf.check_events(aliens_settings,screen,ship,bullets)
-        gf.update_screen(aliens_settings,screen,ship,bullets)#更新屏幕上的图像,并切换到新屏幕
+        gf.update_screen(aliens_settings,screen,ship,bullets,aliens)#更新屏幕上的图像,并切换到新屏幕
 run_game()
--- Alien_Invasion/game_functions.py
+++ Alien_Invasion/game_functions.py
@@ -1,6 +1,7 @@
 import sys,pygame
 from ship import Ship
 from bullet import Bullet
+from alien import Alien
 
 
 def check_events(aliens_settings,screen,ship,bullets):
@@ -13,6 +14,8 @@ def check_events(aliens_settings,screen,ship,bullets):
             check_events_keyup(event,ship)
 
 def check_events_keydown(event,ship,aliens_settings,screen,bullets):
+    if event.key==pygame.K_q:
+        sys.exit()
     if event.key==pygame.K_SPACE:
         ship.fire=True
     if event.key==pygame.K_RIGHT:
@@ -29,11 +32,50 @@ def check_events_keyup(event,ship):
     if event.key==pygame.K_LEFT:
         ship.moving_left=False
 
-def update_screen(aliens_settings,screen,ship,bullets):
+def get_number_aliens_x(aliens_settings,alien_width):
+    available_space_x=aliens_settings.screen_width-2*alien_width
+    number_aliens_x=int(available_space_x/(2*alien_width))
+    return number_aliens_x
+
+def get_number_rows(aliens_settings,ship_height,alien_height):
+    available_space_y=(aliens_settings.screen_height-(3*alien_height)-ship_height)
+    number_rows=int(available_space_y/(2*alien_height))
+    return number_rows
+
+def create_alien(aliens_settings,screen,aliens,alien_number,number_row):
+    alien=Alien(aliens_settings,screen)
+    alien_width=alien.rect.width
+    alien.x=alien_width+2*alien_width*alien_number
+    alien.rect.x=alien.x
+    alien.rect.y=alien.rect.height+2*alien.rect.height*number_row
+    aliens.add(alien)
+
+def create_fleet(aliens_settings,screen,aliens,ship):
+    alien=Alien(aliens_settings,screen)
+    number_aliens_x=get_number_aliens_x(aliens_settings,alien.rect.width)
+    number_rows=get_number_rows(aliens_settings,ship.rect.height,alien.rect.height)
+    for alien_number in range(number_aliens_x):
+        for number_row in range(number_rows):
+            create_alien(aliens_settings,screen,aliens,alien_number,number_row)
+
+def check_fleet_edges(aliens_settings,aliens):
+    for alien in aliens.sprites():
+        if alien.check_edges():
+            change_feet_direction(aliens_settings,aliens)
+            break
+def change_feet_direction(aliens_settings,aliens):
+    for alien in aliens.sprites():
+        alien.rect.y+=aliens_settings.fleet_drop_speed
+    aliens_settings.fleet_direction*=-1
+
+def update_screen(aliens_settings,screen,ship,bullets,aliens):
     screen.fill(aliens_settings.bg_color)
     ship.update()
     ship.blitme()
     bullets.update()
+    check_fleet_edges(aliens_settings,aliens)
+    aliens.update()
+    aliens.draw(screen)
     for bullet in bullets.copy():
         if bullet.rect.bottom<=0:
             bullets.remove(bullet)
